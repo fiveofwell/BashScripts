39b1b0f64fa2e39825747a079e3c351b0ea9da41
fiveofwell <fiveofwell777@gmail.com>
2025-04-15 23:23:43 +0900

    Add support for specifying IP address

--- check_dhcp_allocation.sh
+++ check_dhcp_allocation.sh
@@ -29,9 +29,15 @@ scan_start_ip="192.168.1.%g"
 
 if [[ $# -eq 1 ]];then
 	check_ip_format ${1}
-elif [[ $# -eq 2 ]]; then
-	check_ip_format ${1}
+	scan_start_ip=${1}
+elif [[ $# -ne 0 ]];then
+	echo "Too many arguments."
+	exit 1
 fi
+
+scan_start_ip="$(echo ${scan_start_ip} | cut -d . -f 1-3).0"
+echo "Scan starts from: ${scan_start_ip}"
+scan_start_ip="$(echo ${scan_start_ip} | cut -d . -f 1-3).%g";
 
 seq -f "${scan_start_ip}" 254 | xargs -P 100 -I {} bash -c \
 	'ping -c 1 {} | grep "ttl" | cut -d " " -f 4 | sed "s/://g" > '"$temp_dir"'/{}result.txt'
