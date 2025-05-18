b76cc8dc448a0dcedb4f4c4445cee8a5eef36c82
fiveofwell <fiveofwell777@gmail.com>
2025-05-18 23:50:40 +0900

    Added ping test skip option

--- portscan.sh
+++ portscan.sh
@@ -19,30 +19,50 @@ scan_ports=(
 	8080
 )
 
+skip_ping=false
+
 if [[ $# -eq 0 ]]; then
 	address="192.168.1.1"
 	check_IP_format "${address}"
 elif [[ $# -eq 1 ]]; then
-	address="$1"
+	if [[ "$1" == "-s" ]]; then
+		skip_ping=true
+		address="192.168.1.1"
+		check_IP_format "${address}"
+	else
+		address="$1"
+		check_IP_format "${address}"
+	fi
+elif [[ $# -eq 2 ]]; then
+	option="$1"
+	if [[ "${option}" == "-s" ]]; then
+		skip_ping=true
+	else
+		echo "Invalid option."
+		echo "Usage: $0 [-s] [IP address]"
+		exit 1
+	fi
+	address="$2"
 	check_IP_format "${address}"
 else
 	echo "Too many arguments."
 	exit 1
 fi
 
-if ping -c 1 -W 3 "${address}" >/dev/null 2>&1; then
-	echo "The port scan is performed on ${address}."
-
-	for port in "${scan_ports[@]}"
-	do
-		if nc -z -w1 "${address}" "${port}" 2>/dev/null; then
-			echo "${port} is open."
-		else
-			echo "${port} is closed."
-		fi
-	done
-else
-	echo "Cannot access to ${address}"
-	exit 1
+if ! ${skip_ping}; then
+	if ! ping -c 1 -W 3 "${address}" >/dev/null 2>&1; then
+		echo "Cannot access to ${address}"
+		exit 1
+	fi
 fi
 
+echo "The port scan is performed on ${address}."
+
+for port in "${scan_ports[@]}"
+do
+	if nc -z -w1 "${address}" "${port}" 2>/dev/null; then
+		echo "${port} is open."
+	else
+		echo "${port} is closed."
+	fi
+done
