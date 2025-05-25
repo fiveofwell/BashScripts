c8c40e08cb4563588ec533720642b4ae16920e87
fiveofwell <fiveofwell777@gmail.com>
2025-05-25 12:07:28 +0900

    Add script: traceroute.sh

---
+++ traceroute.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source check_IP_format.sh
+
+address=$1
+max_hops=30
+
+if [[ -z ${address} ]]; then
+	echo "Usage: $0 [address]"
+	exit 1
+fi
+
+check_IP_format "${address}"
+
+echo "${max_hops} hops max."
+
+for ttl in $(seq 1 ${max_hops})
+do
+	result=$(ping -c1 -t "${ttl}" "${address}")
+	if [[ $? -eq 0 ]]; then
+		echo "${ttl} : ${address}"
+		break
+	elif [[ -n $(echo "${result}" | grep exceeded) ]]; then
+		reply_address=$(echo "${result}" | grep From | cut -d ' ' -f 2)
+		echo "${ttl} : ${reply_address}"
+	else
+		echo "${ttl} : *"
+	fi
+done
