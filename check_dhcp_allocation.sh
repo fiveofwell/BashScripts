4a41ad9ed6534dbeeffa12e33d816c85376f7086
fiveofwell <fiveofwell777@gmail.com>
2025-06-14 20:53:09 +0900

    refactor(check_dhcp_allocation): quote variables properly

--- check_dhcp_allocation.sh
+++ check_dhcp_allocation.sh
@@ -8,14 +8,14 @@ function usage () {
 	echo "IP_address    Scan start address"
 }
 
-temp_dir=$(mktemp -d)
+temp_dir="$(mktemp -d)"
 
-scan_start_ip="192.168.1.%g"
+scan_start_ip="192.168.1.0"
 
-if [[ $# -eq 1 ]];then
+if [[ "$#" -eq 1 ]]; then
 	check_IP_format "${1}"
-	scan_start_ip=${1}
-elif [[ $# -ne 0 ]];then
+	scan_start_ip="${1}"
+elif [[ "$#" -ne 0 ]]; then
 	echo "Too many arguments."
 	usage
 	exit 1
@@ -26,7 +26,7 @@ echo "Scan starts from: ${scan_start_ip}"
 scan_start_ip="$(echo "${scan_start_ip}" | cut -d . -f 1-3).%g";
 
 seq -f "${scan_start_ip}" 254 | xargs -P 100 -I {} bash -c \
-	'ping -c 1 {} | grep "ttl" | cut -d " " -f 4 | sed "s/://g" > '"$temp_dir"'/{}result.txt'
+	'ping -c 1 {} | grep "ttl" | cut -d " " -f 4 | sed "s/://g" > '"${temp_dir}"'/{}result.txt'
 
-cat ${temp_dir}/*result.txt | sort -u
-rm -r ${temp_dir}
+sort -u "${temp_dir}"/*result.txt
+rm -r "${temp_dir}"
