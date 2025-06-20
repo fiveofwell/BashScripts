b785fc81af554da08dabc0fffe4901ae273cbef4
fiveofwell <fiveofwell777@gmail.com>
2025-06-20 22:43:32 +0900

    refactor(traceroute): quote variables properly

--- traceroute.sh
+++ traceroute.sh
@@ -7,17 +7,17 @@ function usage () {
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/check_IP_format.sh"
 
-if [[ $# -eq 0 ]]; then
+if [[ "$#" -eq 0 ]]; then
 	echo "Target IP address required."
 	usage
 	exit 1
-elif [[ $# -ge 2 ]]; then
+elif [[ "$#" -ge 2 ]]; then
 	echo "Too many arguments."
 	usage
 	exit 1
 fi
 
-address=$1
+address="$1"
 max_hops=30
 
 check_IP_format "${address}"
@@ -33,14 +33,14 @@ reached=false
 
 for ttl in $(seq 1 ${max_hops})
 do
-	result=$(LANG=C ping -c1 -t "${ttl}" "${address}")
-	if [[ $? -eq 0 ]]; then
-		response_time=$(echo "$result" | grep 'time=' | sed -E 's/.*time=([0-9.]+) ms.*/\1/')
+	result="$(LANG=C ping -c1 -t "${ttl}" "${address}")"
+	if [[ "$?" -eq 0 ]]; then
+		response_time="$(echo "$result" | grep 'time=' | sed -E 's/.*time=([0-9.]+) ms.*/\1/')"
 		echo "${ttl} : ${address} (${response_time} ms)"
 		reached=true
 		break
-	elif [[ -n $(echo "${result}" | grep exceeded) ]]; then
-		reply_address=$(echo "${result}" | grep 'From' | cut -d ' ' -f 2)
+	elif [[ -n "$(echo "${result}" | grep exceeded)" ]]; then
+		reply_address="$(echo "${result}" | grep 'From' | cut -d ' ' -f 2)"
 		echo "${ttl} : ${reply_address}"
 	else
 		echo "${ttl} : *"
