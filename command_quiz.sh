1e60e7e8635395cb9b58b2a91c133e1e9a4e8631
fiveofwell <fiveofwell777@gmail.com>
2025-07-07 19:42:19 +0900

    feat(command_quiz): add result display

--- command_quiz.sh
+++ command_quiz.sh
@@ -56,16 +56,35 @@ i=0
 echo "Command quiz!"
 echo "Type \"end\" to finish"
 
+correct_cnt=0
+problem_cnt=0
+
 while [[ "${input}" != "end" && "${i}" -lt "${#problems[@]}" ]]
 do
 	cmd="${problems["${i}"]}"
 	description="$(man "${cmd}" | col -b | sed -n '/^NAME/,/^SYNOPSIS/p' | grep '-' | cut -d '-' -f 2 | sed 's/^ //g' )"
+	echo ""
 	echo "\"${description}\""
 	read input
 	if [[ "${input}" == "${cmd}" ]];then
 		echo "Correct!"
+		(( correct_cnt++ ))
 	elif [[ "${input}" != "end" ]]; then
 		echo "Incorrect. The answer is ${cmd}"
+	else
+		(( problem_cnt-- ))
 	fi
 	(( i++ ))
+	(( problem_cnt++ ))
 done
+
+echo ""
+echo "==RESULT=="
+if [[ "${correct_cnt}" -eq "${problem_cnt}" ]]; then
+	echo "${correct_cnt}/${problem_cnt} PERFECT!"
+else
+	echo "${correct_cnt}/${problem_cnt}"
+fi
+
+accuracy="$(( correct_cnt * 100 / problem_cnt ))"
+echo "Accuracy: ${accuracy}%"
