bde413f772c885b4d704275eb8aa1b8aa216c068
fiveofwell <fiveofwell777@gmail.com>
2025-08-05 23:05:25 +0900

    feat(check_hash): add -y (auto_yes) and -n (auto_no) options to skip interactive input

--- check_hash.sh
+++ check_hash.sh
@@ -24,6 +24,50 @@ add_hash () {
 
 }
 
+interactive_add_hash () {
+	local append_file="$1"
+	local auto_yes="$2"
+	local auto_no="$3"
+
+	skip="false"
+	if [[ ! -f "${append_file}" ]]; then
+		echo "File ${append_file} does not exist."
+		exit 1
+	elif grep -Fq "${append_file}" "${FILENAME}"; then
+		if [[ "${auto_yes}" = "true" ]]; then
+			echo "The hash of ${append_file} will be replaced."
+			if ! remove_hash "${append_file}"; then
+				exit 1
+			fi
+		elif [[ "${auto_no}" = "true" ]]; then
+			echo "The hash of ${append_file} was already recorded. Process skipped."
+			skip="true"
+		else	
+			echo "The hash of ${append_file} was already recorded."
+			while true 
+			do
+				echo -n "Would you like to replace the hash? (yes/no): "
+				read -r input
+				if [[ "${input,,}" = "yes" ]]; then
+					if ! remove_hash "${append_file}"; then
+						exit 1
+					fi
+					break
+				elif [[ "${input,,}" = "no" ]]; then
+					skip="true"
+					break
+				else
+					echo "Invalid input: ${input}"
+				fi
+			done
+		fi
+	fi
+
+	if [[ "${skip}" = "false" ]]; then
+		add_hash "${append_file}"
+	fi
+}
+
 remove_hash () {
 	local remove_file="$1"
 	if [[ ! -f "${remove_file}" ]]; then
@@ -47,39 +91,17 @@ remove_hash () {
 mkdir -p "${FILE_DIR}"
 touch "${FILENAME}"
 
-while getopts "a:r:p" OPT
+flag_a="false"
+flag_p="false"
+auto_yes="false"
+auto_no="false"
+
+while getopts "a:r:pyn" OPT
 do
 	case "${OPT}" in
 		a)
 			append_file="${OPTARG}"
-			skip="false"
-			if [[ ! -f "${append_file}" ]]; then
-				echo "File ${append_file} does not exist."
-				exit 1
-			elif grep -Fq "${append_file}" "${FILENAME}"; then
-				echo "The hash of ${append_file} is already recorded."
-				while true 
-				do
-					echo -n "Would you like to replace the hash? (yes/no): "
-					read -r input
-					if [[ "${input,,}" = "yes" ]]; then
-						if ! remove_hash "${append_file}"; then
-							exit 1
-						fi
-						break
-					elif [[ "${input,,}" = "no" ]]; then
-						skip="true"
-						break
-					else
-						echo "Invalid input: ${input}"
-					fi
-				done
-			fi
-
-			if [[ "${skip}" != "true" ]]; then
-				add_hash "${append_file}"
-			fi
-			exit 0
+			flag_a="true"
 			;;
 		r)
 			if remove_hash "${OPTARG}"; then
@@ -89,8 +111,21 @@ do
 			fi
 			;;
 		p)
-			cat "${FILENAME}"
-			exit 0
+			flag_p="true"
+			;;
+		y)
+			if [[ "${auto_no}" == "true" ]]; then
+				echo "The -y and -n options are mutually exclusive."
+				exit 1
+			fi
+			auto_yes="true"
+			;;
+		n)
+			if [[ "${auto_yes}" == "true" ]]; then
+				echo "The -y and -n options are mutually exclusive."
+				exit 1
+			fi
+			auto_no="true"
 			;;
 		*)
 			usage
@@ -105,6 +140,21 @@ if [[ "$#" -ne 0 ]]; then
 	echo "Unexpected arguments: $@"
 	usage
 	exit 1
+fi
+
+if [[ "${flag_p}" = "true" ]]; then
+	cat "${FILENAME}"
+	exit 0
+fi
+
+if [[ "${flag_a}" = "false" && ("${auto_yes}" = "true" || "${auto_no}" = "true") ]]; then
+	echo "The -y or -n option must be used with the -a option."
+	exit 1
+fi
+
+if [[ "${flag_a}" = "true" ]]; then
+	interactive_add_hash "${append_file}" "${auto_yes}" "${auto_no}"
+	exit 0
 fi
 
 while read line
